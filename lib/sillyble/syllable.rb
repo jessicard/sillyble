8b35df139de25483040bc585b4ae8c2b922c688c
Colin Pilloud <colin.pilloud@gmail.com>
2014-10-18 22:04:40 -0600

    Implement a more advanced heuristic using regex and some simple rules

--- lib/sillyble/syllable.rb
+++ lib/sillyble/syllable.rb
@@ -3,7 +3,11 @@ require 'sillyble'
 module Sillyble
   class Syllable
     def self.syllable_count(word)
-      word.scan(/[aeiou]/).count
+      word.downcase!
+      return 1 if word.length <= 3
+      word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+      word.sub!(/^y/, '')
+      word.scan(/[aeiouy]{1,2}/).size
     end
   end
 end
--- spec/sillyble_spec.rb
+++ spec/sillyble_spec.rb
@@ -1,7 +1,11 @@
 require 'sillyble'
 
 describe Sillyble::Syllable do
-  it "broccoli is gross" do
+  it "a real simple word should have a real simple syllable count" do
     Sillyble::Syllable.syllable_count("Hello").should eql(2)
+  end
+
+  it "a little more advanced word requires a more complex syllable count" do
+    Sillyble::Syllable.syllable_count("balloon").should eql(2)
   end
 end
