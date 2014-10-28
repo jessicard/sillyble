31009d258821769a965de3b568fc4ed1453a1c9f
Colin Pilloud <colin.pilloud@gmail.com>
2014-10-28 09:47:26 -0600

    Add unit test for each one syllable word

--- lib/sillyble/syllable.rb
+++ lib/sillyble/syllable.rb
@@ -8,12 +8,12 @@ module Sillyble
     Zlib::GzipReader.open(File.expand_path("../dict.tar.gz", __FILE__)) do |gz|
       dict = gz.read
       dict.split("\n").each do |line|
-        dict_def = line.split(": ")
+        dict_def = line.split(":")
         @@syllable_map[dict_def[0]] = dict_def[1]
       end
     end
 
-    def self.syllable_count(word)
+    def self.count(word)
       word.downcase!
       return @@syllable_map[word].scan("-").count + 1 if @@syllable_map[word]
       return 1 if word.length <= 3
--- spec/sillyble_spec.rb
+++ spec/sillyble_spec.rb
@@ -1,11 +1,26 @@
 require 'sillyble'
 
+def load_fixture(fixture_name)
+  File.open(File.expand_path("../fixtures/#{fixture_name}", __FILE__)) do |f|
+    yield f
+  end
+end
+
 describe Sillyble::Syllable do
   it "a real simple word should have a real simple syllable count" do
-    Sillyble::Syllable.syllable_count("Hello").should eql(2)
+    Sillyble::Syllable.count("Hello").should eql(2)
   end
 
   it "a little more advanced word requires a more complex syllable count" do
-    Sillyble::Syllable.syllable_count("balloon").should eql(2)
+    Sillyble::Syllable.count("balloon").should eql(2)
+  end
+
+  load_fixture("one-syllable.txt") do |f|
+    f.each_line do |word|
+      word.chomp!
+      it "#{word} should be marked as having one syllable" do
+        Sillyble::Syllable.count(word).should eql(1)
+      end
+    end
   end
 end
