52f1b46eddcb1e0c0b65e6d986da9e6639ccc850
Colin Pilloud <colin.pilloud@gmail.com>
2014-11-02 20:39:09 -0700

    rename syllable fixtures to allow for easier testing in loop

--- spec/sillyble_spec.rb
+++ spec/sillyble_spec.rb
@@ -8,19 +8,22 @@ end
 
 describe Sillyble::Syllable do
   it "a real simple word should have a real simple syllable count" do
-    Sillyble::Syllable.count("Hello").should eql(2)
+    Sillyble::Syllable.count("hello").should eql(2)
   end
 
   it "a little more advanced word requires a more complex syllable count" do
     Sillyble::Syllable.count("balloon").should eql(2)
   end
 
-  load_fixture("one-syllable.txt") do |f|
-    f.each_line do |word|
-      word.chomp!
-      it "#{word} should be marked as having one syllable" do
-        Sillyble::Syllable.count(word).should eql(1)
+  1.upto(2).each do |i|
+    load_fixture("#{i}-syllable.txt") do |f|
+      f.each_line do |word|
+        word.chomp!
+        it "#{word} should be marked as having #{i} syllable(s)" do
+          Sillyble::Syllable.count(word).should eql(i)
+        end
       end
     end
   end
+
 end
